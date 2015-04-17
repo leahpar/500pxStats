f733b29ca6ed75bc3319656bc62072725efb5aea
leahpar <leahpar@gmail.com>
2015-04-18 00:34:39 +0200

    handle exceptions in updateAll

--- src/PX500/CoreBundle/Services/DataService.php
+++ src/PX500/CoreBundle/Services/DataService.php
@@ -45,66 +45,63 @@ class DataService
 
         foreach($users as $user)
         {
-            // Update user
-            $this->log("Update user");
-            $photosCount = $user->getPhotosCount();
-            $minFromLastUpdate = $user->getDelayLastUpdate();
-            $userStat = $this->updateUser($user);
+            try {
+                // Update user
+                $this->log("Update user");
+                $photosCount = $user->getPhotosCount();
+                $minFromLastUpdate = $user->getDelayLastUpdate();
+                $userStat = $this->updateUser($user);
 
-            $this->log("Last update $minFromLastUpdate min ago");
-
-            // save stat
-            if ($minFromLastUpdate > 10)
-            {
-                $this->log("persist stat");
-                $em->persist($userStat);
-            }
-
-            // Get new photo
-            $this->log("Check new photo");
-            if ($user->getPhotosCount() > $photosCount)
-            {
-                $this->log("Get new photo");
-                $photo = $this->getPhoto($user);
-                $user->addPhoto($photo);
-                $em->persist($photo);
-            }
-
-            // Update photos
-            $this->log("Update photos");
-            $photos = $user->getPhotos();
-            foreach ($photos as $photo)
-            {
-                $minFromUpload = $photo->getDelay();
-                $minFromLastUpdate = $photo->getDelayLastUpdate();
                 $this->log("Last update $minFromLastUpdate min ago");
-                $delay = 0;
 
-                if ($minFromUpload < 30)
-                {
-                    $delay = 1;
-                }
-                else if ($minFromUpload < 2000)
-                {
-                    $delay = 5;
-                }
-                else
-                {
-                    $delay = -1;
+                // save stat
+                if ($minFromLastUpdate > 10) {
+                    $this->log("persist stat");
+                    $em->persist($userStat);
                 }
 
-                // time to update
-                if ($delay > 0 && $minFromLastUpdate > $delay)
-                {
-                    $this->log("Time to update");
-                    $photoStat = $this->getPhotoStats($photo);
-                    $photo->addStat($photoStat);
-                    $em->persist($photoStat);
+                // Get new photo
+                $this->log("Check new photo");
+                if ($user->getPhotosCount() > $photosCount) {
+                    $this->log("Get new photo");
+                    $photo = $this->getPhoto($user);
+                    $user->addPhoto($photo);
+                    $em->persist($photo);
                 }
-                else
-                {
-                    $this->log("No update");
+
+                // Update photos
+                $this->log("Update photos");
+                $photos = $user->getPhotos();
+                foreach ($photos as $photo) {
+                    try {
+                        $minFromUpload = $photo->getDelay();
+                        $minFromLastUpdate = $photo->getDelayLastUpdate();
+                        $this->log("Last update $minFromLastUpdate min ago");
+                        $delay = 0;
+
+                        if ($minFromUpload < 30) {
+                            $delay = 1;
+                        } else if ($minFromUpload < 2000) {
+                            $delay = 5;
+                        } else {
+                            $delay = -1;
+                        }
+
+                        // time to update
+                        if ($delay > 0 && $minFromLastUpdate > $delay) {
+                            $this->log("Time to update");
+                            $photoStat = $this->getPhotoStats($photo);
+                            $photo->addStat($photoStat);
+                            $em->persist($photoStat);
+                        } else {
+                            $this->log("No update");
+                        }
+                    } catch (HttpException $e) {
+                        // continue next photo
+                    }
                 }
+            } catch (HttpException $e) {
+                // continue next user
             }
         }
         $this->log("The end");
